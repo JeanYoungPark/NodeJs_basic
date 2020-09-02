95172a057d2ce1ba2d9f9311c0a950065d8c6c63
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-09-02 20:20:28 +0900

    test2

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -3,7 +3,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({ //mysql연결
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : '123456',
   database : 'opentutorials'
 });
   
